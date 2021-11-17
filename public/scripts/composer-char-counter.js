f7128e49c8c27bb05ede4746dbe4120e1c5ca28a
WenConrad <conradwen@gmail.com>
2021-11-16 21:33:23 -0800

    tweet chracter count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-  console.log("WE READY BOIS");
+  $("#tweet-text").on('input', function() {
+    const charCount = this.nextElementSibling.childNodes[3];
+    charCount.value = 140 - this.value.length;
+    if (charCount.value < 0) {
+      charCount.addClass("lengthExceeded");
+    }
+  });
 });
